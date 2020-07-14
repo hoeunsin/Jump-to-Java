b99e91ace15691da7c6b88770a8dd74930584d47
Eun-ho <hoeunsin@gmail.com>
2020-07-14 18:15:54 +0900

    again

--- ch_03_5_StringBuffer_02_insert.java
+++ ch_03_5_StringBuffer_02_insert.java
@@ -5,8 +5,8 @@ public class ch_03_5_StringBuffer_02_insert {
 		StringBuffer sb = new StringBuffer();
 		sb.append("jump to java");
 		String hello = "hello";
-		int hello_length = hello.length();
-		sb.insert(0, hello);
+		int hello_length = hello.length();  // string의 length를 알려주는 메소드
+		sb.insert(0, hello);  // String 추가할 수 있는 메소드. 위치 먼저 지정, 그리고 문자열
 		sb.insert(hello_length,  " ");
 		System.out.println(sb.toString());
 	}
